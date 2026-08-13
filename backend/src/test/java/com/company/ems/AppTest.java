f4cf1d79c157cb492646514bdc5034b12f7dcda0
Moriarty <mohamednasser221b@gmail.com>
2026-08-13 17:05:29 +0300

    snyk

---
+++ backend/src/test/java/com/company/ems/AppTest.java
@@ -0,0 +1,13 @@
+package com.company.ems;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AppTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
